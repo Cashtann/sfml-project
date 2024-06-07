2a89f0046c7ed3d8490024db1c51bb919a166a3a
cashtann <switalamaciej5@gmail.com>
2024-06-07 21:44:35 +0200

    test commit

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+cd build 
+make
+cd ..
+
 cd bin/Linux/Debug  
 # directory, where the executable file is located, may differ if compiling in Release mode 
 # or Debug mode, also depends on OS
--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,21 @@
 
 int main()
 {
-    sf::RenderWindow window(
-        sf::VideoMode(640, 480),
-        "Hello, World");
-    sf::CircleShape shape(200);
-
+    sf::RenderWindow window(sf::VideoMode(640, 480), "SFML Application");
+    sf::CircleShape shape;
+    shape.setRadius(40.f);
+    shape.setPosition(100.f, 100.f);
+    shape.setFillColor(sf::Color::Cyan);
     while (window.isOpen())
     {
         sf::Event event;
-        while (
-            window.pollEvent(event))
-            if (event.type ==
-                sf::Event::Closed)
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
                 window.close();
-
+        }
         window.clear();
         window.draw(shape);
         window.display();
     }
-    return 0;
 }
